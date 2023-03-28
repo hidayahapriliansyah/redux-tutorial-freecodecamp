bf26dcb09e6e83938eac6228b383f9c4382b353a
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-28 14:43:18 +0700

    cart-slice-state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
-  const isLoggedIn = useSelector((state) => state.authentication.isLoggedIn);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
     <div className="App">
       {isLoggedIn ? <Layout /> : <Auth /> }
