c3da4e4249e7fcffa97e05e1c50793a98cdabee9
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-29 13:10:08 +0700

    sending-async-http-request-with-redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from 'react-redux';
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch('https://redux-freecodecamp-a8c58-default-rtdb.firebaseio.com/cartItems.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+      const data = res.json();
+    };
+    sendRequest();
+  }, [cart]);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
     <div className="App">
